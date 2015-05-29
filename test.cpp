ac1a67765ac735d7046fa100612fa42934e31ac3
Cory Knapp <knapp.cory@gmail.com>
2015-05-29 14:55:37 -0700

    Wrote a macro to do unit tests.

--- test.cpp
+++ test.cpp
@@ -1,60 +1,30 @@
 #include "Command.h"
 //clang++ -std=c++11 test.cpp
 
+#define TEST( name, exp, result ) {\
+	std::cout << "Testing " << name << std::endl;\
+	if( exp == result )\
+		std::cout << "\tsuccess!" << std::endl; \
+	else{ \
+		std::cout << #exp << " returns " << exp << std::endl; \
+	} \
+}
+
+
 int main(int argc, char *argv[])
 {
 	std::string testString( "test { a , b , c , [foo : 1, bar : 2 ] }" );
-	std::cout << "VVVVVVVVV" << std::endl;
 	std::cout << "running tests on Command with init string:" << std::endl
 		<< "\t" << testString << std::endl;
 	Command testCmd( testString );
 	std::cout <<"Reseralizes as " << testCmd.text() << std::endl;
 
-
-	std::cout << "Testing type()" << std::endl;
-	if( testCmd.type() == "test" )
-		std::cout<<"\tsuccess!" << std::endl;
-	else
-		std::cout << "\tfail :(" << std::endl;
-
-
-	std::cout << "Testing getValueAt(\"0\")" << std::endl;
-	if( testCmd.getValueAt<std::string>("0") == "a" )
-		std::cout<<"\tsuccess!" << std::endl;
-	else{
-		std::cout << "\tfail :(" << std::endl;
-		std::cout << "getValueAt<std::string>(\"0\") returns \""
-		  << testCmd.getValueAt<std::string>("0") << "\"" << std::endl;
-	}
-
-	std::cout << "Testing getValueAt(\"1\")" << std::endl;
-	if( testCmd.getValueAt<std::string>("1") == "b" )
-		std::cout<<"\tsuccess!" << std::endl;
-	else{
-		std::cout << "\tfail :(" << std::endl;
-		std::cout << "getValueAt<std::string>(\"1\") returns \""
-		  << testCmd.getValueAt<std::string>("1") << "\"" << std::endl;
-	}
-
-
-	std::cout << "Testing getValueAt(\"3/foo\")" << std::endl;
-	if( testCmd.getValueAt<std::string>("3/foo") == "1" )
-		std::cout<<"\tsuccess!" << std::endl;
-	else{
-		std::cout << "\tfail :(" << std::endl;
-		std::cout << "getValueAt<std::string>(\"3/foo\") returns \""
-		  << testCmd.getValueAt<std::string>("3/foo") << "\"" << std::endl;
-	}
-
-	std::cout << "Testing getValueAt(\"3/bar\")" << std::endl;
-	if( testCmd.getValueAt<std::string>("3/bar") == "2" )
-		std::cout<<"\tsuccess!" << std::endl;
-	else{
-		std::cout << "\tfail :(" << std::endl;
-		std::cout << "getValueAt<std::string>(\"3/bar\") returns \""
-		  << testCmd.getValueAt<std::string>("3/bar") << "\"" << std::endl;
-	}
-
+	TEST( "type", testCmd.type(), "test" );
+	TEST( "get value at 1", testCmd.getValueAt<std::string>("1"), "b" );
+	TEST(	"get value at 3/foo",
+			testCmd.getValueAt<std::string>("3/foo"), "1" );
+	TEST(	"get value at 3/bar",
+			testCmd.getValueAt<std::string>("3/bar"), "2" );
 
 	return 0;
 }
