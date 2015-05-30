8cfeaf4f0d62012be404be44ea8ff562d37bdb15
Cory Knapp <knapp.cory@gmail.com>
2015-05-30 16:08:15 -0700

    Added tests for implicit lists

--- test.cpp
+++ test.cpp
@@ -14,19 +14,21 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	string testString( "test { a , b , c , [foo : 1, bar : 2 ] }" );
+	//string testString = "test { a , b , c , [foo : 1, bar : 2 ] }";
+	string testString = "request [target:1]";
 	cout << "running tests on Command with init string:" << endl
 		<< "\t" << testString << std::endl;
 	Command testCmd( testString );
 	cout <<"Reseralizes as " << testCmd.text() << endl;
 
 	TEST( "type", testCmd.type(), "test" );
-	TEST( "get value at 1", testCmd.getValueAt<std::string>("1"), "b" );
+	TEST(	"get value at 1", *testCmd.getStringAt("1"), "b" );
 	TEST(	"get value at 3/foo",
-			testCmd.getValueAt<std::string>("3/foo"), "1" );
+			*testCmd.getStringAt("3/foo"), "1" );
 	TEST(	"get value at 3/bar",
-			testCmd.getValueAt<std::string>("3/bar"), "2" );
+			*testCmd.getStringAt("3/bar"), "2" );
 
-
+	string implicidListString = "test x y x";
+	 
 	return 0;
 }
